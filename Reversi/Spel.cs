779e8fed8b543d27a67e183cf676e40820940926
YouriiZ <youri@zwanepol.nl>
2013-10-07 13:18:22 +0200

    1st push

--- Reversi/Spel.cs
+++ Reversi/Spel.cs
@@ -12,13 +12,29 @@ namespace Reversi
 {
     public partial class Spel : Form
     {
+        const int x = 6;
+        const int y = 6;
+        int[,] board = new int[x,y];
+
         public Spel()
         {
             InitializeComponent();
+
+            this.ClientSize = new Size(x * 50 + 100, y * 50 + 50);
+
+            int centerX = x / 2;
+            int centerY = y / 2;
+
+            board[centerX, centerY] = 1;
+            board[centerX, centerY + 1] = 2;
+            board[centerX + 1, centerY] = 2;
+            board[centerX + 1, centerY + 1] = 2;            
         }
 
         private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            
+
 
         }
 
