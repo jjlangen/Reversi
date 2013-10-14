14730742ad9dd73cad4d86abaf748ec8114d6ea8
YouriiZ <youri@zwanepol.nl>
2013-10-15 00:15:22 +0200

    Added more comments

--- Reversi/Game.cs
+++ Reversi/Game.cs
@@ -25,7 +25,7 @@ namespace Reversi
         // Constants x and y affect rows and colums on the board, d is the field size
         const int x = 6;
         const int y = 6;
-        const int d = 50;
+        const int d = 60;
         #endregion
 
         #region Member variables
@@ -70,6 +70,7 @@ namespace Reversi
 
         private void paintBoard(Graphics g, int circleSize)
         {
+            // Draw white tiles
             for (int i = 0; i < x; i++)
             {
                 for (int j = 0; j < y; j++)
@@ -79,6 +80,7 @@ namespace Reversi
                 }
             }
 
+            // Draw lines
             for (int i = 0; i <= x; i++)
                 g.DrawLine(Pens.Black, i * d + d, d, i * d + d, y * d + d);
 
@@ -92,13 +94,16 @@ namespace Reversi
             {
                 for (int j = 0; j < y; j++)
                 {
+                    // Draw all pieces, 1 = red, 2 = blue
                     if (board[i, j] != 0)
                         g.FillEllipse(board[i, j] == 1 ? Brushes.Red : Brushes.Blue, i * d + d + 1, j * d + d + 1, circleSize, circleSize);
+                    // If 'Help' is pressed, show all possible locations for a new piece
                     else if (pressedHelp && validLocations[i, j] == 1)
                         g.FillEllipse(Brushes.Yellow, i * d + d + 1, j * d + d + 1, circleSize, circleSize);
                 }
             }
 
+            // Reset pressedHelp to false
             if (pressedHelp)
                 pressedHelp = false;
         }
@@ -110,6 +115,7 @@ namespace Reversi
             rect1 = new Rectangle(d, y * d + 2 * d, circleSize, circleSize);
             rect2 = new Rectangle(x * d, y * d + 2 * d, circleSize, circleSize);
 
+            // Draw two circles with the score in it
             g.FillEllipse(Brushes.Red, rect1);
             g.FillEllipse(Brushes.Blue, rect2);
             g.DrawString(calculateScore(1).ToString(), f, Brushes.White, rect1, sf);
@@ -151,10 +157,13 @@ namespace Reversi
         #region Gameplay functions
         private void processClick(object sender, MouseEventArgs mea)
         {
+            /* Get the coordinates of the mouseclick, we're doing some complicated casting to avoid clicks directly left to the board to
+             * being interpreted as 0 (and thus the first column) instead of -1 */
             int coordX = (int)Math.Floor((double)(mea.X - d) / d);
             int coordY = (int)Math.Floor((double)(mea.Y - d) / d);
             int opponent = activePlayer == 1 ? 2 : 1;
 
+            // Switch turns if the player needed to pass
             if (state == Status.pass)
                 switchTurns();
             else
@@ -163,6 +172,7 @@ namespace Reversi
 
         private void addPiece(int coordX, int coordY, int[,] validLocations, int opponent)
         {
+            // First check if the location of the click is within bounds, then if it's a valid location
             if (isWithinBounds(coordX, coordY) && validLocations[coordX, coordY] == 1)
             {
                 passCounter = 0;
@@ -181,11 +191,13 @@ namespace Reversi
             return flip(coordX, coordY, opponent, false);
         }
 
+        // Get valid locations fills an array that is used to determine if a click is valid AND counts the number of valid locations
         private int getValidLocations()
         {
             int numberOfValidLocations = 0;
             int opponent = activePlayer == 1 ? 2 : 1;
 
+            // Reinitialise array
             validLocations = new int[x, y];
 
             for (int i = 0; i < x; i++)
@@ -203,8 +215,9 @@ namespace Reversi
             return numberOfValidLocations;
         }
         
-        // Flip() flips the pieces which are between the clicked field and the active player's pieces, if the parameter flip is set to true.
-        // If the parameter flip is set to false, it will just return true or false without flipping.
+        /* Flip() determines if a location is valid by checking the requirements (one or more pieces of the opponent followed by a piece 
+         * of the active player) in all 8 directions. If the parameter 'flip' is set to true it will also flip the piece of the opponent.
+         * We decided to use one method for both actions, because otherwise we had to write two methods that would be very much alike */
         private bool flip(int coordX, int coordY, int opponent, bool flip)
         {
             // Define possible operations (directions)
@@ -242,7 +255,7 @@ namespace Reversi
                         // Finding a piece of the active player after one or more of the opponent means the location is valid
                         else if (board[localX, localY] == activePlayer)
                         {
-                            // Flip the pieces if function is called with flip = true
+                            // Flip the pieces if function is called with flip = true, otherwise return true
                             if (flip)
                             {
                                 // Flip the pieces from end --> start
@@ -250,19 +263,21 @@ namespace Reversi
                                     board[localX, localY] = activePlayer;
                                 break;
                             }
-
-                            return true;
+                            else
+                            {
+                                return true;
+                            }
                         }
-                        // If an opponent's piece is found we'll just keep looping in the current direction, searching for one of the active player
+                        // If an opponent's piece is found we'll just keep looking in the current direction, searching for one of the active player
                     }
                 }
             }
 
-            // If all directions are checked and none did satisfy our requirements (one or more pieces of an opponent, after that a piece of the active player), 
-            // we'll return false at last
+            // If all directions are checked and none did satisfy our requirements we'll return false at last
             return false;
         }
 
+        // Sets and resets all variables necessary for a new game
         private void startNewGame(bool firstGame)
         {
             const int centerX = x / 2;
@@ -313,7 +328,6 @@ namespace Reversi
             {
                 int red, blue;
                 if ((red = calculateScore(1)) > (blue = calculateScore(2)))
-
                     state = Status.winred;
                 else if (red < blue)
                     state = Status.winblue;
@@ -324,6 +338,7 @@ namespace Reversi
             this.Invalidate();
         }
 
+        // Calculate score by walking over the complete board
         private int calculateScore(int player)
         {
             int res = 0;
